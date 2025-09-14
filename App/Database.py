aba8931a89f68effeae952f2bef378a1ca0f4c0a
agutor <1997.agustorres@gmail.com>
2025-09-14 20:19:57 -0300

    Función para acceder a db implementada

--- App/Database.py
+++ App/Database.py
@@ -9,3 +9,9 @@ engine = create_engine(URL_BASE_DATOS, echo=True, future=True)
 SesionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False, future=True)
 Base = declarative_base()
 
+def get_db():
+    db = SesionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
