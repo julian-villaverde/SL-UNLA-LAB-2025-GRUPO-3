6774ab8c5660c4105a8e9572a5ccf8ffdc82e125
agutor <1997.agustorres@gmail.com>
2025-09-14 21:47:05 -0300

    fix comment

--- App/Database.py
+++ App/Database.py
@@ -9,6 +9,7 @@ engine = create_engine(URL_BASE_DATOS, echo=True, future=True)
 SesionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False, future=True)
 Base = declarative_base()
 
+#Acceder a la base de datos
 def get_db():
     db = SesionLocal()
     try:
